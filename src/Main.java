a4aa0cb105f1176fb607031e8c8adb449dac0c8c
Kpeguero16 <khalilpeguero@hotmail.com>
2022-08-17 23:35:07 -0400

    Some new ideas added to the TO-DO

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,10 @@ public class Main {
          * TO-DO:
          * Get user input for name, pages, and days
          * Decide wether to make a GUI for the app or leave it as a command line program
+         * MAYBE: use book api for book look up instead of making the user input the info themselves or give both options 
+         * Create something to store user's information maybe using some type of login or storing values in local storage 
+         * Digital Library, book ranking system similar to what I have for movies in Notion. Basically MyAnimeList but for books
+         * ^_^
          */
 
         Book newBook = new Book("The Gathering Storm", 752, 30);
