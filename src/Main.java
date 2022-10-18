abdf117c195781e0cd5dcb012f34a65a98b429ba
Kpeguero16 <khalilpeguero@hotmail.com>
2022-10-18 13:36:56 -0400

    Still figuring things out

--- src/Main.java
+++ src/Main.java
@@ -3,35 +3,59 @@ package src;
 import java.awt.Color;
 import java.awt.Font;
 
-import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.border.Border;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.plaf.DimensionUIResource;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        // Border border = BorderFactory.createLineBorder(new Color(255, 250, 250), 2);
+
+
+
+
 
         JLabel header = new JLabel();
         header.setText("Book Calculator");
         header.setForeground(new Color(255, 239, 213));
-        header.setFont(new Font("Times New Roman", Font.BOLD, 32));
-        // header.setBorder(border);
+        header.setFont(new Font("Times New Roman", Font.BOLD, 38));
         header.setHorizontalAlignment(JLabel.CENTER);
         header.setVerticalAlignment(JLabel.CENTER);
         header.setBounds(0, 0, 450, 100);
 
+        JLabel q1 = new JLabel("Welcome!\nWhat book do you want to read?");
+        q1.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        q1.setForeground(new Color(255, 239, 213));
+        q1.setHorizontalAlignment(JLabel.CENTER);
+
+        JTextField nameOfBook = new JTextField();
+        nameOfBook.setPreferredSize(new DimensionUIResource(250, 30));
+        nameOfBook.setBackground(Color.white);
+        nameOfBook.setHorizontalAlignment(JLabel.CENTER);
+        nameOfBook.setFont(new Font("Times New Roman", Font.PLAIN, 16));
+        
+
+        JPanel panel = new JPanel();
+        panel.setBounds(0, 0, 450, 150);
+        panel.setBackground(new Color(70, 130, 180));
+        panel.add(header);
+        panel.add(q1);
+        panel.add(nameOfBook);
+
         JFrame frame = new JFrame();
         frame.setTitle("Book Calculator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setResizable(false);
+        frame.setResizable(true);
         frame.setSize(450, 800);
         frame.setLayout(null);
-        frame.setVisible(true);
         frame.getContentPane().setBackground(new Color(70, 130, 180));
-        frame.add(header);
+        frame.add(panel);
+        frame.setVisible(true);
+
         /*
          * TO-DO:
          * Decide wether to make a GUI for the app or leave it as a command line program
@@ -45,7 +69,6 @@ public class Main {
         */
         
         System.out.println("\nHi! Welcome to the book calculator ^_^\n");
-        
         Scanner scanner = new Scanner(System.in);
         System.out.print("What book do you want to read?\n");
         String bookInput = scanner.nextLine();
@@ -60,6 +83,7 @@ public class Main {
         
         System.out.println(newBook);
         
+    
 
     }
 }
